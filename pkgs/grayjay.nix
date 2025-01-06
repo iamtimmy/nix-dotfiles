21e455798fd189f0fbfae56a5b33dd582884dde8
iamtimmy <58427647+iamtimmy@users.noreply.github.com>
2025-01-06 13:06:20 +0100

    update grayjay

--- pkgs/grayjay.nix
+++ pkgs/grayjay.nix
@@ -1,18 +1,13 @@
 {
-  stdenv,
+  buildDotnetModule,
+  fetchgit,
+  dotnetCorePackages,
+  buildNpmPackage,
   lib,
-  fetchurl,
-  buildFHSEnv,
-  writeShellScript,
-
-  unzip,
-
   libz,
   icu,
   openssl,
-
   xorg,
-
   gtk3,
   glib,
   nss,
@@ -30,50 +25,65 @@
   mesa,
   libGL,
   libsecret,
+  gitUpdater,
 }:
-
 let
-  grayjay-app = stdenv.mkDerivation {
-    pname = "grayjay-app";
-    version = "0.1.0";
-
-    src = fetchurl {
-      url = "https://updater.grayjay.app/Apps/Grayjay.Desktop/Grayjay.Desktop-linux-x64.zip";
-      hash = "sha256-MU4AJVKkE80/E9ikq9RKIL9Z3npwmC2N0PHybd++jQM=";
-    };
-
-    buildInputs = [
-      unzip
-    ];
-
-    sourceRoot = ".";
-
-    dontUnpack = true;
-    dontConfigure = true;
-    dontBuild = true;
-
+  version = "3";
+  src = fetchgit {
+    url = "https://gitlab.futo.org/videostreaming/Grayjay.Desktop";
+    tag = version;
+    hash = "sha256-gI1M/rDTSH16eeknHJfLA2iawoA5iGTKHfwUW4/lvv8=";
+    fetchSubmodules = true;
+    fetchLFS = true;
+  };
+  frontend = buildNpmPackage {
+    name = "grayjay-frontend";
+    inherit version src;
+    sourceRoot = "Grayjay.Desktop/Grayjay.Desktop.Web";
+    npmBuildScript = "build";
+    npmDepsHash = "sha256-pTEbMSAJwTY6ZRriPWfBFnRHSYufSsD0d+hWGz35xFM=";
     installPhase = ''
-      mkdir -p $out/bin $out/share
-      unzip -d $out $src && f=($out/*) && mv $out/*/* $out
-      rm $out/Portable
+      runHook preInstall
+      cp -r dist/ $out
+      runHook postInstall
     '';
-
-    meta = with lib; {
-      homepage = "https://grayjay.app/desktop/";
-      description = "Grayjay Desktop";
-      platforms = platforms.linux;
-    };
   };
 in
-buildFHSEnv {
-  name = "grayjay";
-  targetPkgs = pkgs: [
-    grayjay-app
-
+buildDotnetModule {
+  pname = "grayjay";
+  inherit version src;
+  projectFile = [
+    "Grayjay.ClientServer/Grayjay.ClientServer.csproj"
+    "Grayjay.Engine/Grayjay.Engine/Grayjay.Engine.csproj"
+    "Grayjay.Desktop.CEF/Grayjay.Desktop.CEF.csproj"
+    "FUTO.MDNS/FUTO.MDNS/FUTO.MDNS.csproj"
+    "JustCef/DotCef.csproj"
+  ];
+  testProjectFile = [
+    "Grayjay.Desktop.Tests/Grayjay.Desktop.Tests.csproj"
+    "Grayjay.Engine/Grayjay.Engine.Tests/Grayjay.Engine.Tests.csproj"
+  ];
+  nugetDeps = ./grayjay_deps.json;
+  dotnet-runtime = dotnetCorePackages.aspnetcore_8_0;
+  executables = [ "Grayjay" ];
+  preBuild = ''
+    rm -r Grayjay.ClientServer/wwwroot/web
+    cp -r ${frontend} Grayjay.ClientServer/wwwroot/web
+  '';
+  postInstall = ''
+    chmod +x $out/lib/grayjay/cef/dotcefnative
+    rm $out/lib/grayjay/Portable
+    ln -s /tmp/grayjay-launch $out/lib/grayjay/launch
+    ln -s /tmp/grayjay-cef-launch $out/lib/grayjay/cef/launch
+  '';
+  makeWrapperArgs = [
+    "--chdir"
+    "${placeholder "out"}/lib/grayjay"
+  ];
+  runtimeDeps = [
     libz
     icu
     openssl # For updater
-
     xorg.libX11
     xorg.libXcomposite
     xorg.libXdamage
@@ -81,7 +91,6 @@ buildFHSEnv {
     xorg.libXfixes
     xorg.libXrandr
     xorg.libxcb
-
     gtk3
     glib
     nss
@@ -100,12 +109,14 @@ buildFHSEnv {
     libGL
     libsecret
   ];
-
-  multiPkgs = pkgs: [ libGL ];
-  runScript = writeShellScript "grayjay-script" ''
-    mkdir -p ~/Grayjay
-    cp -r ${grayjay-app}/* ~/Grayjay/
-    cd ~/Grayjay
-    ./Grayjay
-  '';
+  passthru.updateScript = gitUpdater { };
+  meta = {
+    description = "Multi-platform media application that allows you to watch content from multiple platforms in a single application";
+    longDescription = ''Grayjay is a multi-platform media application that allows you to watch content from multiple platforms in a single application. Using an extendable plugin system developers can make new integrations with additional platforms. Plugins are cross-compatible between Android and Desktop.'';
+    homepage = "https://grayjay.app/desktop/";
+    # license = lib.licenses.sfl;
+    maintainers = with lib.maintainers; [ samfundev ];
+    platforms = lib.platforms.linux;
+    mainProgram = "Grayjay";
+  };
 }
